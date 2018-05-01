5757d627a409dc45630a1a9f34568aac5d93f40c
Tim O'Grady <Tim.OGrady@anthonynolan.org>
2018-05-01 10:39:50 +0100

    guinea fowl

---
+++ Zoo.BusinessLogic/Models/Animals/GuineaFowl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.BusinessLogic.Models.Animals
+{
+    public class GuineaFowl : Animal
+    {
+        public GuineaFowl(DateTime dateOfBirth) : base(dateOfBirth)
+        {
+        }
+    }
+}
